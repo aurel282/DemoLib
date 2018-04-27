03a1c6272c2d207031406d6f8447c62656b54a1d
Aurel <aurelien.gek@gmail.com>
2018-04-28 00:45:52 +0200

    First template commit

---
+++ SortAlgo/Algo.py
@@ -0,0 +1,55 @@
+####    Comparaison sort algorithm  ####
+
+
+def quicksort():
+    print("StartFastSort")
+
+def mergesort():
+
+def inplacemergesort():
+
+def heapsort():
+
+
+def insertionsort():
+
+def introsort():
+
+def selectionsort():
+
+def timsort():
+
+def cubesort():
+
+def shellsort():
+
+def bubblesort():
+
+def binarytreesort():
+
+def cyclesort():
+
+def librarysort():
+
+def partiencesorting():
+
+def smoothsort():
+
+def strandsort():
+
+def tournamentsort():
+
+def cockailsortsort():
+
+def combsort():
+
+def gnomesort():
+
+def unshufflesort():
+
+def franceschinismethodsort():
+
+def blocksort():
+
+def oddevensort():
+
